d7dcd62fb7e2271e74c40bde8961c71b2f3a639a
Gourav Wadhwan <30312776+gourav1125@users.noreply.github.com>
2017-08-22 00:01:41 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,212 @@
+<?php include ('inc/header.php'); ?>
+	<section class="slider" id="slider">
+		<div class="slItems">
+			<div class="slItem" style="background-image: url('images/Banner1.jpg');background-position: center center; background-size:cover"></div>
+			<div class="slItem" style="background-image: url('images/Banner2.jpg');background-position: center center;background-size:cover"></div>
+			<div class="slItem" style="background-image: url('images/Banner3.jpg');background-position: center center;background-size:cover"></div>
+			<div class="slItem" style="background-image: url('images/Banner4.jpg');background-position: center center;background-size:cover"></div>
+		</div>
+	</section>
+	<div class="clearfix"></div>
+	<section class="about-us">
+		<div class="container">
+			<div class="row">
+				<div class="appoint">
+					<form>
+						<h4>Make AN APPOINTMENT</h4>
+						<div class="col-md-4 col-sm-6 form-group">
+							<input type="text" placeholder="FULL NAME" class="form-control">
+						</div>
+						<div class="col-md-4 col-sm-6 form-group">
+							<input type="text" placeholder="Contact Number" class="form-control">
+						</div>
+						<div class="col-md-4 col-sm-6 form-group">
+							<input type="text" placeholder="Email Id" class="form-control">
+						</div>
+						<div class="col-md-4 col-sm-6 form-group">
+							<input type="text" placeholder="DATE" id="datepicker" class="form-control"><i class="fa fa-calendar" aria-hidden="true"></i>
+						</div>
+						<div class="col-md-4 col-sm-6 form-group">
+							<div class="form-group">
+								<select id="basic1" class="selectpicker show-tick form-control">
+									<option>DEPARTMENT</option>
+									<option>bull</option>
+									<option class="get-class" disabled>ox</option>
+								</select>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-6 form-group">
+							<button class="btn btn-default book" type="submit">book now</button>
+						</div>
+					</form>
+				</div>
+				<div class="clearfix"></div>
+				<div class="about about-us">
+					<h1>About Our Clinic</h1>
+					<span class="red-line"></span>
+					<h5>One stop solution for all your hair care needs</h5>
+					<p>Derma Clinix is one of the leading hair transplant centers in India serving to the needs of numerous people in need of effective hair care. Our long experience in the industry and dealing with thousands of patients have put us in a better position to device the most effective solution to grow back your hair – surgical or non-surgical.</p>
+					<p>Our treatment has given a new life to many men and women on whom even doctors had given up! We cater to all age groups providing successful hair loss treatment and wide range of hair transplant services. We are here to answer all your questions and give you the best possible way to get a head full of hair!</p>
+					<div class="button">
+						<a href="about.php" class="btn btn-default book">Know More</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="clearfix"></div>
+	<section class="hair-clinic">
+		<div class="container">
+			<div class="row">
+				<h1>Why Choose Us?</h1>
+				
+				<span class="red-line"></span>
+				<div class="col-sm-6 col-md-4 boxes wow_imgage wow fadeInLeft animated" data-wow-delay="0.1s" style="visibility: visible;-webkit-animation-delay: 0.1s; -moz-animation-delay: 0.1s; animation-delay: 0.1s;">
+					<img src="images/doctor.png" alt="doctor">
+					<h4>Qualified Team</h4>
+					<p>We have roped in the best professionals and experts from the field with long years of exposure and experience.</p>
+				</div>
+				<div class="col-sm-6 col-md-4 boxes wow_imgage wow fadeInLeft animated" data-wow-delay="0.3s" style="visibility: visible;-webkit-animation-delay: 0.3s; -moz-animation-delay: 0.3s; animation-delay: 0.3s;">
+					<img src="images/price.png" alt="price">
+					<h4>Machinery and Equipment</h4>
+					<p>Our clinic is equipped with the latest technological and medical equipments to give you the optimum results.</p>
+				</div>
+				<div class="col-sm-6 col-md-4 boxes wow_imgage wow fadeInLeft animated" data-wow-delay="0.5s" style="visibility: visible;-webkit-animation-delay: 0.5s; -moz-animation-delay: 0.5s; animation-delay: 0.5s;">
+					<img src="images/team.png" alt="team">
+					<h4>Infrastructure</h4>
+					<p>Our state of the art facility is designed to provide the best setup necessary to give our best towards your hair care needs.</p>
+				</div>
+				<div class="col-sm-6 col-md-4 boxes wow_imgage wow fadeInRight animated" data-wow-delay="0.5s" style="visibility: visible;-webkit-animation-delay: 0.5s; -moz-animation-delay: 0.5s; animation-delay: 0.5s;">
+					<img src="images/quality.png" alt="quality">
+					<h4>Our Achievements</h4>
+					<p>We have provided hope to numerous men and women and helped them get back natural looking strong hair- their smile is our biggest achievement.</p>
+				</div>
+				<div class="col-sm-6 col-md-4 boxes wow_imgage wow fadeInRight animated" data-wow-delay="0.3s" style="visibility: visible;-webkit-animation-delay: 0.3s; -moz-animation-delay: 0.3s; animation-delay: 0.3s;">
+					<img src="images/emergancy.png" alt="emergancy">
+					<h4>Operation Theatres</h4>
+					<p>Our advanced operation theatres are equipped with all medical resources, equipments, medications and treatment tools necessary to carry out a successful operation.</p>
+				</div>
+				<div class="col-sm-6 col-md-4 boxes wow_imgage wow fadeInRight animated" data-wow-delay="0.1s" style="visibility: visible;-webkit-animation-delay: 0.1s; -moz-animation-delay: 0.1s; animation-delay: 0.1s;">
+					<img src="images/client.png" alt="client">
+					<h4>Our Results</h4>
+					<p>Our efforts and high success rates are the main cause behind our reputation in the industry which sustained us as one of the top leaders in hair care.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="clearfix"></div>
+	<section class="about blog">
+		<div class="container">
+			<div class="row">
+				<h1 class="wow_imgage wow fadeInIn animated" data-wow-delay="0.5s" style="visibility: visible;-webkit-animation-delay: 0.5s; -moz-animation-delay: 0.5s; animation-delay: 0.5s;">LATEST FROM THE BLOG</h1>
+				<span class="red-line"></span>
+				<h5 class="wow_imgage wow fadeInDown animated" data-wow-delay="0.5s" style="visibility: visible;-webkit-animation-delay: 0.5s; -moz-animation-delay: 0.5s; animation-delay: 0.5s;">Neque porro quisquam est qui dolor sit amet consectetur non numquam eius </h5>
+				<div class="col-sm-6 wow_imgage wow fadeInLeft animated" data-wow-delay="0.5s" style="visibility: visible;-webkit-animation-delay: 0.5s; -moz-animation-delay: 0.5s; animation-delay: 0.5s;">
+					<div class="blog-box">
+						<div class="blog-img">
+							<img src="images/blog-1.jpg" alt="blog">
+						</div>
+						<div class="blog-text">
+							<h4>5 ways to prevent female hair loss</h4>
+							<p>25 September 2016</p>
+							<h6>Reasons for hair loss in women are many including anaemia, pregnancy and stress. Treatment for hair loss in women ...</h6>
+							<button style="margin-top:20px;" type="submit" class="btn btn-default book">Know More</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 wow_imgage wow fadeInRight animated" data-wow-delay="0.5s" style="visibility: visible;-webkit-animation-delay: 0.5s; -moz-animation-delay: 0.5s; animation-delay: 0.5s;">
+					<div class="blog-box">
+						<div class="blog-img">
+							<img src="images/blog-2.jpg" alt="blog">
+						</div>
+						<div class="blog-text">
+							<h4>Scalp Micro-pigmentation for Men</h4>
+							<p>25 September 2016</p>
+							<h6>Micro-pigmentation is a solution for complete baldness as well as to cover crown balding. Suitable for people of different colours…</h6>
+							<button type="submit" class="btn btn-default book">Know More</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="clearfix"></div>
+	<section class="testimonial" id="carousel">
+		<div class="container">
+			<div class="row">
+				<div class="testi-box col-md-10 col-md-offset-1">
+					<div class="quote"><i><img src="images/quotes.png" alt="quotes"></i></div>
+					<h1>What our customers say</h1>
+					<span class="red-line"></span>
+					<div class=" col-md-10 col-md-offset-1">
+						
+						<div class="carousel slide" id="fade-quote-carousel" data-ride="carousel" data-interval="3000">
+						  <!-- Carousel indicators -->
+						  <ol class="carousel-indicators">
+							<li data-target="#fade-quote-carousel" data-slide-to="0" class="active"></li>
+							<li data-target="#fade-quote-carousel" data-slide-to="1"></li>
+							<li data-target="#fade-quote-carousel" data-slide-to="2" ></li>
+							<li data-target="#fade-quote-carousel" data-slide-to="3" ></li>
+							<li data-target="#fade-quote-carousel" data-slide-to="4" ></li>
+							<li data-target="#fade-quote-carousel" data-slide-to="5" ></li>
+							<li data-target="#fade-quote-carousel" data-slide-to="6" ></li>
+							<li data-target="#fade-quote-carousel" data-slide-to="7" ></li>
+						  </ol>
+						  <!-- Carousel items -->
+						  <div class="carousel-inner">
+							<div class="active item">
+								<blockquote>
+									<p>“Good and experienced Doctor. He is very friendly and cooperative and advised easy to follow remedies. Thank you, Dermaclinix for giving me my confidence back.”</p>
+									<h2>Vedprakash</h2>
+								</blockquote>	
+							</div>
+							<div class="item">
+								<blockquote>
+									<p>“Treatment was satisfactory and fairly detailed. Doctor attentively heard all my issues before recommending the required tests and medication.”</p>
+									<h2>Neha Agarwal</h2>
+								</blockquote>
+							</div>
+							<div class=" item">
+								<blockquote>
+									<p>“Doctor and the team both are good. Though the treatment is still going on, I am feeling better. Hope to get good results. Thanks”</p>
+									<h2>Sanjay Kumar</h2>
+								</blockquote>
+							</div>
+							<div class=" item">
+								<blockquote>
+									<p>“No false promises made, saw progress before the expected time specified at the first meeting and steady progress thereafter.”</p>
+									<h2>Apala Saha</h2>
+								</blockquote>
+							</div>
+							<div class=" item">
+								<blockquote>
+									<p>“Overall experience was good. Doctor suggested me the best way to repair my bald areas. I am completely satisfied.”</p>
+									<h2>Amit Thakur</h2>
+								</blockquote>
+							</div>
+							<div class=" item">
+								<blockquote>
+									<p>“Doctor Ariganesh is soft spoken and caring. He invested a good time on my treatment, taking care of every minute thing.”</p>
+									<h2>Brijmohan</h2>
+								</blockquote>
+							</div>
+							<div class=" item">
+								<blockquote>
+									<p>“I underwent hair transplant surgery 6 months back and I am completely satisfied with the results. I would recommend Dr.Ariganesh to anyone suffering for hair loss problem.”</p>
+									<h2>Jitender</h2>
+								</blockquote>
+							</div>
+							<div class=" item">
+								<blockquote>
+									<p>“Dr Ariganesh very experienced and I am fully satisfied my HT surgery done under his supervision. I appreciated his dedication and services.”</p>
+									<h2>A.R Krishna Raj</h2>
+								</blockquote>
+							</div>
+						  </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<?php include ('inc/footer.php'); ?>
